fe337ca64dea6645311635f13f5e55c3f651c72a
Satyanarayan parida <Satyanarayanparida12@gmail.com>
2025-10-24 14:42:47 +0530

    Added project files

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+const time = document.querySelectorAll(".time");
+const chantBtn = document.querySelector(".chantBtn");
+const btnMusic = document.querySelector(".btn-music");
+const btnreset = document.querySelector(".btn-reset");
+
+let count = 0;
+
+// time update Function
+function updateTime() {
+  time.forEach((span, index) => {
+    const padCount = count.toString().padStart(4, "0");
+    span.textContent = padCount[index];
+  });
+}
+
+// update time
+
+chantBtn.addEventListener("click", () => {
+  count = count > 9999 ? 0 : ++count;
+  updateTime();
+
+  chantAudio.currentTime = 0;
+  chantAudio.play();
+});
+
+// Reset time
+
+btnreset.addEventListener("click", () => {
+  count = 0;
+  updateTime();
+});
+
+// Music
+
+// Chant
+const chantAudio = new Audio("../Src/music/Radhe Radhe.mp3");
+const bgMusic = new Audio(
+  "../Src/music/hare-krishna-relaxing-music-5-117834.mp3"
+);
+
+// backgroundMusic setup  (play,pause,loop)
+let bgMusicPlaying = false;
+bgMusic.loop = true;
+
+btnMusic.addEventListener("click", () => {
+  if (!bgMusicPlaying) {
+    bgMusic.play();
+    bgMusicPlaying = true;
+  } else {
+    bgMusic.pause();
+    bgMusicPlaying = false;
+  }
+});
